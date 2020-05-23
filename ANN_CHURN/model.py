26725eff8111ec78244b728165a6d24f95ec3dac
Jai Singh <jai.si8004@gmail.com>
2020-05-23 15:35:11 +0530

    First Commit

---
+++ ANN_CHURN/model.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+# In[2]:
+
+
+
+# Importing the dataset
+dataset = pd.read_csv('Churn_Modelling.csv')
+X = dataset.iloc[:, 3:13]
+y = dataset.iloc[:, 13]
+
+
+# In[3]:
+
+
+
+#Create dummy variables
+geography=pd.get_dummies(X["Geography"],drop_first=True)
+gender=pd.get_dummies(X['Gender'],drop_first=True)
+
+
+# In[4]:
+
+
+
+## Concatenate the Data Frames
+
+X=pd.concat([X,geography,gender],axis=1)
+
+## Drop Unnecessary columns
+X=X.drop(['Geography','Gender'],axis=1)
+
+
+# In[5]:
+
+
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+
+# In[6]:
+
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+
+# In[7]:
+
+
+
+
+# Importing the Keras libraries and packages
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LeakyReLU,PReLU,ELU
+from keras.layers import Dropout
+
+
+# In[8]:
+
+
+classifier = Sequential()
+classifier.add(Dense(output_dim = 6, init = 'he_uniform',activation='relu',input_dim = 11))
+classifier.add(Dense(output_dim = 6, init = 'he_uniform',activation='relu'))
+classifier.add(Dense(output_dim = 1, init = 'glorot_uniform', activation = 'sigmoid'))
+classifier.compile(optimizer = 'Adamax', loss = 'binary_crossentropy', metrics = ['accuracy'])
+model_history=classifier.fit(X_train, y_train,validation_split=0.33, batch_size = 10, nb_epoch = 100)
+
+
+# In[9]:
+
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+y_pred = (y_pred > 0.5)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+# Calculate the Accuracy
+from sklearn.metrics import accuracy_score
+score=accuracy_score(y_pred,y_test)
+
+
+# In[21]:
+
+
+score
+
+
+# In[23]:
+
+
+f= open("ann_acc.txt","w+")
+f.write(str(round(score*100,2)))
+f.close()
+print("Accuracy of model is = " , round(score*100,2) ,"%")
+
+
+# In[ ]:
+
+
+
+
